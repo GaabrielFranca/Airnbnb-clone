97fc8a8af4c76a80154814f3fddd31161bb4464a
Gabriel França <gabrielfran1864@gmail.com>
2022-11-06 10:16:11 -0300

    calendar implementing guests number

--- components/Header.js
+++ components/Header.js
@@ -6,14 +6,17 @@ import {
   GlobeAltIcon,
   Bars3Icon,
   UserCircleIcon,
+  UsersIcon,
 } from "@heroicons/react/24/solid";
 import "react-date-range/dist/styles.css";
 import "react-date-range/dist/theme/default.css";
 
 const Header = () => {
   const [searchInput, setSearchInput] = React.useState("");
+
   const [startDate, setStartDate] = React.useState(new Date());
   const [endDate, setEndtDate] = React.useState(new Date());
+  const [guests, setGuests] = React.useState(1);
 
   function handleSelect(ranges) {
     setStartDate(ranges.selection.startDate);
@@ -26,7 +29,7 @@ const Header = () => {
   };
 
   return (
-    <header className="grid grid-cols-3 p-5 bg-white  shadow-md sticky  top-0 z-10 md:px-10">
+    <header className="grid grid-cols-3 p-5 bg-white  shadow-md sticky  top-0 z-10 md:px-10 ">
       <div className="cursor-pointer my-auto relative flex h-10">
         <Image
           priority={true}
@@ -65,6 +68,30 @@ const Header = () => {
             ranges={[selectionRange]}
             rangeColors={["#FD4B61"]}
           />
+          <div className="flex items-center border-b mb-4">
+            <h2 className="flex-grow text-2xl font-semibold">
+              Number of Guests
+            </h2>
+            <UsersIcon className="h-5" />
+            <input
+              min={1}
+              onChange={({ target }) => setGuests(target.value)}
+              value={guests}
+              type="number"
+              className="w-12 pl-2 text-lg outline-none text-red-400"
+            />
+          </div>
+          <div className="flex">
+            <button
+              className="flex-grow text-gray-500"
+              onClick={() => {
+                setSearchInput("");
+              }}
+            >
+              Cancel
+            </button>
+            <button className="flex-grow text-red-400">Search</button>
+          </div>
         </div>
       )}
     </header>
